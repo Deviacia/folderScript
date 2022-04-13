9821c54c581512d34c698d7c575de3129ecaa8eb
Deviacia <ivan.bpemr@gmail.com>
2022-04-13 21:47:49 +0300

    Script is completed

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 // Project settings
-const dirSrc = './banners'
+const dirExport = './banners'
 const templates = [
     {w: 300, h: 300},
-    {w: 200, h: 420}
+    {w: 200, h: 420},
+    {w: 300, h: 420},
+    {w: 600, h: 420}
 ]
 
-// Project settings END
-// 
 // Script
-
 const fs = require('fs')
 
 // Creating banners directory
 const createDir = () => {
-    fs.mkdir(dirSrc, function(err) {
+    fs.mkdir(dirExport, function(err) {
         if (err) {
           console.log(err)
         } else {
@@ -22,7 +21,7 @@ const createDir = () => {
     })
 
     templates.forEach((item, index) => {
-        fs.mkdir(`./${dirSrc}/${item.w}x${item.h}`, function(err) {
+        fs.mkdir(`./${dirExport}/${item.w}x${item.h}`, function(err) {
             if (err) {
               console.log(err)
             } else {
@@ -35,20 +34,11 @@ const createDir = () => {
 // Creating files and including them in separate directory
 const createFiles = () => {
     fs.readFile("template/index.html", {encoding: 'utf-8'}, (err, data) => {
-        // console.log(data)
-        const dataWidth = '{width}'
-        const dataHeight = '{height}'
-
         templates.forEach((item, index) => {
-          fs.writeFile(`./${dirSrc}/${item.w}x${item.h}/index.html`, data, () =>{})
+          fs.writeFile(`./${dirExport}/${item.w}x${item.h}/index.html`, data.replace(/{width}/g, item.w).replace(/{height}/g, item.h), () => {})
         })
     })
 }
 
-const changeIndex = (data) => {
-    
-}
-
-// createDir()
-// Now we need to change index files for independent
+createDir()
 createFiles()
